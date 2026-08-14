3daafecbf409c77298a7c7215d4367d4d6064c11
XandyGomes <x3.computadores@hotmail.com>
2026-08-14 20:31:17 -0300

    Add Comparacao and Produto classes with basic comparison and multiplication functionality

---
+++ src/main/java/com/alexandre/aulastpdsm20262/Produto.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.alexandre.aulastpdsm20262;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alexandre
+ */
+public class Produto {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int x, y, z, result;
+        
+        System.out.print("Digite o primeiro numero: ");
+        x = input.nextInt();
+        System.out.print("Digite o segundo numero: ");
+        y = input.nextInt();
+        System.out.print("Digite o terceiro numero: ");
+        z = input.nextInt();
+        
+        result = x * y * z;
+        
+        System.out.println("O resultado do produto e: " + result);
+    }
+}
